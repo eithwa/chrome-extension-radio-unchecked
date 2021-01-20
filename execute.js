3d8512253cfc2cdcc3f648af3be76c5fb0d6970b
eithwa <mich841012@gmail.com>
2021-01-20 20:51:29 +0800

    init

---
+++ execute.js
@@ -0,0 +1,32 @@
+var radio_list=[];
+function radio_unchecked(id)
+{
+    let element = document.getElementById(id);
+    if(element.type=="radio")
+    {
+        let checked = false;
+        for(let i=0; i<radio_list.length; i++)
+        {
+            if(element.id==radio_list[i])
+            {
+                checked=true;
+                radio_list.splice(i, 1); 
+                break;   
+            }
+        }
+        if(checked == true)
+        {
+            element.checked=false;
+        }else
+        {
+            radio_list.push(element.id)
+        }
+    }
+}
+$(document).ready(function() {
+    $("input").click(function(event) {
+        //console.log(event.target.id);
+        //console.log(event.target.checked);
+        radio_unchecked(event.target.id);
+    });
+});
